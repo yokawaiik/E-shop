3300e0ed775a3e3d3309a60b34d89f896142f374
Danil Shubin <danil.legioner@gmail.com>
2021-09-15 09:55:48 +0700

    Created Offer for Main activity, adapters and animations.

---
+++ app/src/main/java/com/YoKawaiiK/e_shop/Model/Offer.java
@@ -0,0 +1,11 @@
+package com.YoKawaiiK.e_shop.Model;
+
+public class Offer {
+    public String describtion , img;
+
+    public Offer(String describtion, String img) {
+
+        this.describtion = describtion;
+        this.img = img;
+    }
+}
---
+++ app/src/main/java/com/YoKawaiiK/e_shop/Utils/Constants.java
@@ -0,0 +1,6 @@
+package com.YoKawaiiK.e_shop.Utils;
+
+public class Constants {
+    public static final int COUNT_ITEMS_IN_GRID = 4;
+
+}
