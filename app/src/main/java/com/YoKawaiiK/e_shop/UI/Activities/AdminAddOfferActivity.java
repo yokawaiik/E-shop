abacf936cc12567a045ba19a5193a34dea91ab6e
Danil Shubin <danil.legioner@gmail.com>
2021-12-16 19:55:20 +0700

    Классы для части админ-панели с часть  бизнес-логики.

---
+++ app/src/main/java/com/YoKawaiiK/e_shop/UI/Activities/AdminAddOfferActivity.java
@@ -0,0 +1,282 @@
+package com.YoKawaiiK.e_shop.UI.Activities;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.YoKawaiiK.e_shop.Model.Offer;
+import com.YoKawaiiK.e_shop.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+public class AdminAddOfferActivity extends AppCompatActivity {
+    private TextInputEditText name , description;
+    private Button add , choose;
+    private ImageView img;
+    private Uri imgUri;
+    private StorageReference mStorageRef;
+    private StorageTask mUploadTask;
+    private TextInputLayout nameTextInputLayout , descTextInputLayout;
+    private Toolbar mToolBar;
+    private RelativeLayout CustomCartContainer;
+    private TextView PageTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_offer);
+
+        //tool bar
+        mToolBar = (Toolbar)findViewById(R.id.AddOffer_ToolBar);
+        setSupportActionBar(mToolBar);
+        getSupportActionBar().setTitle("Add Offer");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        name = findViewById(R.id.editTextOfferName);
+        description = findViewById(R.id.editTextOfferDescription);
+        add = findViewById(R.id.btnAddOffer);
+        choose = findViewById(R.id.btnChooseOfferImg);
+        img = findViewById(R.id.offerImage);
+        nameTextInputLayout = findViewById(R.id.editTextOfferLayout);
+        descTextInputLayout = findViewById(R.id.editTextOfferDescriptionLayout);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference("offers");
+
+        nameTextInputLayout.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(name.getText().toString().trim().isEmpty())
+                {
+                    nameTextInputLayout.setErrorEnabled(true);
+                    nameTextInputLayout.setError("Please Enter Offer Name");
+                }
+                else
+                {
+                    nameTextInputLayout.setErrorEnabled(false);
+                }
+            }
+        });
+
+        descTextInputLayout.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(description.getText().toString().trim().isEmpty())
+                {
+                    descTextInputLayout.setErrorEnabled(true);
+                    descTextInputLayout.setError("Please Enter Offer Name");
+                }
+                else
+                {
+                    descTextInputLayout.setErrorEnabled(false);
+                }
+            }
+        });
+
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(name.getText().toString().trim().isEmpty())
+                {
+                    nameTextInputLayout.setErrorEnabled(true);
+                    nameTextInputLayout.setError("Please Enter Offer Name");
+                }
+                else
+                {
+                    nameTextInputLayout.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        description.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(description.getText().toString().trim().isEmpty())
+                {
+                    descTextInputLayout.setErrorEnabled(true);
+                    descTextInputLayout.setError("Please Enter Offer Name");
+                }
+                else
+                {
+                    descTextInputLayout.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mUploadTask != null && mUploadTask.isInProgress())
+                    Toast.makeText(AdminAddOfferActivity.this, "Upload Is In Progress", Toast.LENGTH_SHORT).show();
+                else if(name.getText().toString().isEmpty() || description.getText().toString().isEmpty() || imgUri == null)
+                {
+                    Toast.makeText(AdminAddOfferActivity.this, "Empty Cells", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    try {
+                        uploadData();
+                        Toast.makeText(AdminAddOfferActivity.this, "Added Successfully", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }catch (Exception e)
+                    {
+                        Toast.makeText(AdminAddOfferActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+
+        choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openImage();
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        NotshowCartIcon();
+    }
+
+    public void uploadData()
+    {
+        if(name.getText().toString().isEmpty() || description.getText().toString().isEmpty() || imgUri == null)
+        {
+            Toast.makeText(AdminAddOfferActivity.this, "Empty Cells", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            uploadImage();
+        }
+    }
+
+    public void uploadImage()
+    {
+        if(imgUri != null)
+        {
+            StorageReference fileReference = mStorageRef.child(name.getText().toString() + "." + getFileExtension(imgUri));
+            mUploadTask = fileReference.putFile(imgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Task<Uri> urlTask = taskSnapshot.getStorage().getDownloadUrl();
+                    while (!urlTask.isSuccessful());
+                    Uri downloadUrl = urlTask.getResult();
+                    Offer offer = new Offer(description.getText().toString().trim() ,
+                            downloadUrl.toString());
+                    DatabaseReference z = FirebaseDatabase.getInstance().getReference("offers");
+                    z.child(name.getText().toString().trim()).setValue(offer);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                }
+            });
+        }
+    }
+
+    public void openImage()
+    {
+        Intent i =  new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(i , SignUpActivity.GALARY_PICK);
+    }
+
+    public String getFileExtension(Uri uri)
+    {
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SignUpActivity.GALARY_PICK && resultCode == Activity.RESULT_OK && data != null)
+        {
+            imgUri = data.getData();
+            Log.e("uri" , imgUri.toString());
+            try {
+                Picasso.get().load(imgUri).fit().centerCrop().into(img);
+            } catch (Exception e) {
+                Log.e(this.toString() , e.getMessage().toString());
+            }
+        }
+    }
+
+    private void NotshowCartIcon(){
+        //toolbar & cartIcon
+        ActionBar actionBar= getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+
+        LayoutInflater inflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view= inflater.inflate(R.layout.buyer_toolbar,null);
+        //actionBar.setCustomView(view);
+
+        //************custom action items xml**********************
+        CustomCartContainer = (RelativeLayout)findViewById(R.id.CustomCartIconContainer);
+        PageTitle =(TextView)findViewById(R.id.PageTitle);
+        PageTitle.setVisibility(View.GONE);
+        CustomCartContainer.setVisibility(View.GONE);
+
+    }
+}
---
+++ app/src/main/java/com/YoKawaiiK/e_shop/UI/Activities/EditOfferActivity.java
@@ -0,0 +1,252 @@
+package com.YoKawaiiK.e_shop.UI.Activities;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.YoKawaiiK.e_shop.Model.Offer;
+import com.YoKawaiiK.e_shop.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+public class EditOfferActivity extends AppCompatActivity {
+    private TextInputEditText name, description;
+    private Button edit, choose;
+    private ImageView img;
+    private Uri imgUri;
+    private StorageReference mStorageRef;
+    private DatabaseReference mDatabaseRef;
+    private StorageTask mUploadTask;
+    private String oldName, category , oldImagePath;
+    private byte[] oldImageBytes;
+    private Toolbar mToolBar;
+    private RelativeLayout CustomCartContainer;
+    private TextView PageTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_offer);
+
+        //tool bar
+        mToolBar = (Toolbar)findViewById(R.id.EditOffer_ToolBar);
+        setSupportActionBar(mToolBar);
+        getSupportActionBar().setTitle("Edit Offer");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        name = findViewById(R.id.editTextOfferNameEdit);
+        description = findViewById(R.id.editTextOfferDescriptionEdit);
+        edit = findViewById(R.id.btnEditOffer);
+        choose = findViewById(R.id.btnChooseOfferImgEdit);
+        img = findViewById(R.id.offerImageEdit);
+        Bundle b = getIntent().getExtras();
+        name.setText(b.getString("name"));
+        description.setText(b.getString("describtion"));
+        Picasso.get().load(b.getString("img")).centerCrop().fit().into(img);
+        imgUri = Uri.parse(b.getString("img"));
+
+        oldName = name.getText().toString().trim();
+        oldImagePath = b.getString("img");
+
+
+
+        mStorageRef = FirebaseStorage.getInstance().getReference("offers");
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("offers");
+
+        StorageReference httpsRef = FirebaseStorage.getInstance().getReferenceFromUrl(oldImagePath);
+        final long ONE_MEGABYTE = 1024 * 1024 * 10;
+        httpsRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                oldImageBytes = bytes;
+            }
+        });
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mUploadTask != null && mUploadTask.isInProgress())
+                    Toast.makeText(EditOfferActivity.this, "Upload Is In Progress", Toast.LENGTH_SHORT).show();
+                else {
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(EditOfferActivity.this).setTitle("Confirmation").setMessage("Are You Sure You Want To Save ?!").setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            deleteImage();
+                            deleteData();
+                            uploadData();
+                            finish();
+                        }
+                    }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    }).setIcon(android.R.drawable.ic_dialog_alert);
+                    dialog.show();
+                }
+            }
+        });
+
+        choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openImage();
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        NotshowCartIcon();
+    }
+    public void deleteData() {
+        DatabaseReference reference = mDatabaseRef.child(oldName);
+        reference.removeValue();
+    }
+    public void deleteImage() {
+        if(imgUri.toString().equals(oldImagePath))
+        {
+            StorageReference z = mStorageRef.child(oldName);
+            z.delete();
+        }
+        else
+        {
+            StorageReference z = mStorageRef.child(oldName + ".jpg");
+            z.delete();
+        }
+
+    }
+
+
+
+    public void uploadData() {
+        if (name.getText().toString().isEmpty() || description.getText().toString().isEmpty() || imgUri == null) {
+            Toast.makeText(EditOfferActivity.this, "Empty Cells", Toast.LENGTH_SHORT).show();
+        } else {
+            uploadImage();
+        }
+    }
+
+    public void uploadImage() {
+
+        if(imgUri.toString().equals(oldImagePath))
+        {
+            StorageReference fileReference = mStorageRef.child(name.getText().toString());
+            mUploadTask = fileReference.putBytes(oldImageBytes).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Task<Uri> urlTask = taskSnapshot.getStorage().getDownloadUrl();
+                    while (!urlTask.isSuccessful()) ;
+                    Uri downloadUrl = urlTask.getResult();
+                    Offer offer = new Offer(description.getText().toString().trim(),
+                            downloadUrl.toString());
+                    DatabaseReference z = FirebaseDatabase.getInstance().getReference("offers");
+                    z.child(name.getText().toString().trim()).setValue(offer);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(EditOfferActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        else if (imgUri != null) {
+            StorageReference fileReference = mStorageRef.child(name.getText().toString() + "." + getFileExtension(imgUri));
+            mUploadTask = fileReference.putFile(imgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Task<Uri> urlTask = taskSnapshot.getStorage().getDownloadUrl();
+                    while (!urlTask.isSuccessful()) ;
+                    Uri downloadUrl = urlTask.getResult();
+                    Offer offer = new Offer(description.getText().toString().trim(),
+                            downloadUrl.toString());
+                    DatabaseReference z = FirebaseDatabase.getInstance().getReference("offers");
+                    z.child(name.getText().toString().trim()).setValue(offer);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(EditOfferActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    public void openImage() {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(i, SignUpActivity.GALARY_PICK);
+    }
+
+    public String getFileExtension(Uri uri) {
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SignUpActivity.GALARY_PICK && resultCode == Activity.RESULT_OK && data != null) {
+            imgUri = data.getData();
+            try {
+                Picasso.get().load(imgUri).fit().centerCrop().into(img);
+            } catch (Exception e) {
+                Log.e(this.toString(), e.getMessage().toString());
+            }
+        }
+    }
+
+    private void NotshowCartIcon(){
+        //toolbar & cartIcon
+        ActionBar actionBar= getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+
+        LayoutInflater inflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view= inflater.inflate(R.layout.buyer_toolbar,null);
+        //actionBar.setCustomView(view);
+
+        //************custom action items xml**********************
+        CustomCartContainer = (RelativeLayout)findViewById(R.id.CustomCartIconContainer);
+        PageTitle =(TextView)findViewById(R.id.PageTitle);
+        PageTitle.setVisibility(View.GONE);
+        CustomCartContainer.setVisibility(View.GONE);
+
+    }
+}
